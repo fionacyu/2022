c1c562391d0ff60189476559338e62d4465d26bd
fionacyu <fiona.yu@anu.edu.au>
2022-06-29 11:04:34 +1000

    bo determination fixed

--- load_data.py
+++ load_data.py
@@ -78,9 +78,9 @@ def get_bond_order(atom1, atom2, dist, tol):
             bo = max([k for k in dictionary if dictionary[k][0] - tol <= dist <= dictionary[k][1] + tol])
             return bo
         except ValueError:
-            highest_bo = max(list(dictionary))
-            if dist <= dictionary[highest_bo][0]:
-                return highest_bo
+            boList = [k for k in dictionary if dist < dictionary[k][0]]
+            if boList:
+                return max(boList)
             else:
                 return 0
     
